5adb7477845a37d8c2c14a211a8a637d4363f3a3
Prashant Prithyani <prithyanipd@rknec.edu>
2022-05-13 23:56:38 +0530

    updated

--- email_sender/home/views.py
+++ email_sender/home/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib import messages
+from django.core.mail import send_mail
 
 # Create your views here.
 
 def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        subject = request.POST.get('subject')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
     #return HttpResponse("yoo homepage")
-    messages.success(request, 'Your request was sent.')
-
+        send_mail(
+        subject,
+        message,
+        email,
+        ['prithyani2002@gmail.com'],
+        )
+   
+        return render(request,"index.html",{'name':name})    
+    #messages.success(request, 'Your request was sent.')
     return render(request,'index.html')
