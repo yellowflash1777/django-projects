7dbd1020c260e3fa0a1bc4f1b9f970eb70376ad2
Prashant Prithyani <prithyanipd@rknec.edu>
2022-05-13 23:27:56 +0530

    updated

--- email_sender/home/views.py
+++ email_sender/home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import messages
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("yoo homepage")
+    #return HttpResponse("yoo homepage")
+    messages.success(request, 'Your request was sent.')
+
+    return render(request,'index.html')
