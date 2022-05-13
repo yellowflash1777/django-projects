ce1a049ffdcee671bcb98c4c6a6be105d34dd356
Prashant Prithyani <prithyanipd@rknec.edu>
2022-05-14 02:18:37 +0530

    .

--- email_sender/email_sender/settings.py
+++ email_sender/email_sender/settings.py
@@ -130,6 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT="587"
-EMAIL_HOST_USER="prithyani2002@gmail.com"
-EMAIL_HOST_PASSWORD="kbmemhdejubuvutd"
+EMAIL_HOST_USER=""
+EMAIL_HOST_PASSWORD=""
 EMAIL_USE_TLS = True
